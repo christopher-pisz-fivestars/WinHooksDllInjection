93c8d2e41b6e9bc6164a8f1cd5803782f8b95280
christopher-pisz-fivestars <christopher.pisz@fivestars.com>
2019-07-12 18:10:05 -0500

    [] Error checking
    [](https://fivestars.atlassian.net/browse/)

--- FSHooks/FSHooksLib.cpp
+++ FSHooks/FSHooksLib.cpp
@@ -1,19 +1,20 @@
 #include "pch.h"
+#include "FSHooksLib.h"
 #include "global.h"
 
-extern "C" void Init()
+extern "C" FSHOOKS_API void Init()
 {
     g_log.open("hookslog.txt", std::ofstream::out);
     g_log << "FSHooksLib has been initialized" << std::endl;
 }
 
-extern "C" void Release()
+extern "C" FSHOOKS_API void Release()
 {
     g_log << "FSHooksLib has been releaseded" << std::endl;
     g_log.close();
 }
 
-extern "C" LRESULT HookProc(int code, WPARAM wParam, LPARAM lParam)
+extern "C" FSHOOKS_API LRESULT HookProc(int code, WPARAM wParam, LPARAM lParam)
 {
     if (code != HC_ACTION)
         return CallNextHookEx(nullptr, code, wParam, lParam);
@@ -28,8 +29,10 @@ extern "C" LRESULT HookProc(int code, WPARAM wParam, LPARAM lParam)
     switch (wParam)
     {
     case WM_LBUTTONDOWN:
+        g_log << "Received a WM_LBUTTONDOWN message with injected=" << injected << std::endl;
         break;
     case WM_LBUTTONUP:
+        g_log << "Received a WM_LBUTTONUP message with injected=" << injected << std::endl;
         break;
     default:
         break;
--- Injector/Injector.cpp
+++ Injector/Injector.cpp
@@ -1,13 +1,49 @@
 #include <windows.h>
 
+#include <iostream>
+
 int main()
 {
     HMODULE hMod = LoadLibrary("FSHooks.dll");
+    if (!hMod)
+    {
+        std::cerr << "Count not load the FSHooks library" << std::endl;
+        return -1;
+    }
+
     FARPROC fn_init = GetProcAddress(hMod, "Init");
+    if (!fn_init)
+    {
+        std::cerr << "Count not get the 'Init' function from FSHooks library" << std::endl;
+        return -1;
+    }
+
     FARPROC fn_release = GetProcAddress(hMod, "Release");
-    HHOOK hook = SetWindowsHookEx(WH_KEYBOARD, (HOOKPROC)GetProcAddress(hMod, "HookProc"), hMod, 0);
+    if (!fn_release)
+    {
+        std::cerr << "Count not get the 'Release' function from FSHooks library" << std::endl;
+        return -1;
+    }
+
+    HOOKPROC fn_hook = reinterpret_cast<HOOKPROC>(GetProcAddress(hMod, "HookProc"));
+    if (!fn_hook)
+    {
+        std::cerr << "Count not get the 'HookProc' function from FSHooks library" << std::endl;
+        return -1;
+    }
+
+    HHOOK hook = SetWindowsHookEx(WH_MOUSE_LL, fn_hook, hMod, 0);
+    if (!hook)
+    {
+        std::cerr << "Failed to SetWindowsHookEx" << std::endl;
+        return -1;
+    }
+
+    fn_init();
 
     MSG msg;
+    // TODO - We'll need some manner of signal to terminate
+    //        This is just a quick and dirty example program
     while (true)
     {
         GetMessage(&msg, nullptr, 0, 0);
@@ -16,6 +52,7 @@ int main()
     }
 
     UnhookWindowsHookEx(hook);
+    fn_release();
     FreeLibrary(hMod);
     return 0;
 }
